28f06686f6c64307a702838918ebaaa9ad019985
sumonbarai <sumonbarai78@gmail.com>
2022-05-02 11:15:38 +0600

    carserver

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d4elm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client.db("carproject").collection("services");
+
+    // load all service api GET
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+    // load a single data by using dynamic id
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+
+    // save data to database by post method
+    app.post("/service", async (req, res) => {
+      const data = req.body;
+      const result = await serviceCollection.insertOne(data);
+      res.send(result);
+    });
+
+    // delete data by use id
+    app.delete("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.deleteOne(query);
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`server in running ${port}`);
+});
